46e7668fa64cba6438e568707511f5effcb852f9
patrick brisbin <pbrisbin@gmail.com>
2021-01-20 16:56:45 -0500

    Revert "Supply token with a new entrypoint option"
    This reverts commit 31e835690e9c0aac8645d76af9ed0f70c56df85d.

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 .PHONY: check
 check:
 	docker build --tag freckle/platform-action .
-	docker run -it --rm freckle/platform-action \
-	  -t $(GITHUB_TOKEN) \
-	  -c 'RIO_VERBOSE=1 platform deploy -e dev foo/bar'
+	docker run -it --rm --env-file "$(PWD)"/.env freckle/platform-action \
+	  sh -c 'RIO_VERBOSE=1 platform deploy -e dev foo/bar'
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,59 +1,13 @@
 #!/bin/sh
-set -eu
-
-usage() {
-  cat <<EOM
-Usage: /entrypoint.sh <-t TOKEN> <-c CMD>
-Options:
-  -t TOKEN      GitHub token for downloading Platform CLI asset
-  -c CMD        Command to run, via \`sh -c'
-EOM
-}
-
-token=
-cmd=
-
-while getopts t:c:h opt; do
-  case "$opt" in
-    t)
-      token=$OPTARG
-      ;;
-    c)
-      cmd=$OPTARG
-      ;;
-    h)
-      usage
-      exit 0
-      ;;
-    \?)
-      usage >&2
-      exit 64
-      ;;
-  esac
-done
-shift $((OPTIND - 1))
-
-if [ -z "$token" ]; then
-  echo "-t is required" >&2
-  usage >&2
-  exit 64
-fi
-
-if [ -z "$cmd" ]; then
-  echo "-c is required" >&2
-  usage >&2
-  exit 64
-fi
-
 read -r release </PLATFORM_VERSION
 echo "Installing platform-$release"
 
 tmp=$(mktemp)
 
-if ! platform-setup "$token" "$release" >"$tmp" 2>&1; then
+if ! platform-setup "$release" >"$tmp" 2>&1; then
   echo "Installation failed:" >&2
   cat "$tmp"
   exit 1
 fi
 
-exec sh -c "$cmd"
+exec "$@"
